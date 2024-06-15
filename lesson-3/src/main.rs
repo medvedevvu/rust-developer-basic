3355d2719bc211b7c41f56d3ad1212870f69f155
medvedevvu <medvedevvu72@gmail.com>
2024-06-15 16:41:17 +0300

    домашняя работа к уроку №3 - добавил анализ переполнения

--- lesson-3/src/main.rs
+++ lesson-3/src/main.rs
@@ -4,12 +4,22 @@ fn main() {
     println!("lesson-3 ");
 }
 
-fn double_int32(p: i32) -> i32 {
-    p * 2
+fn double_int32(p: i32) -> (i32, bool) {
+    let (a,b) = p.overflowing_mul(2);
+    if b {
+        (0i32, true)
+    } else {
+        (a, false)
+    }
 }
 
-fn double_int64(p: i32) -> i64 {
-    (p * 2) as i64
+fn double_int64(p: i32) -> (i64, bool) {
+    let (a,b) = p.overflowing_mul(2);
+    if b {
+        (0i64, true)
+    } else {
+        (a as i64 , false)
+    }
 }
 
 fn double_float32(p: f32) -> f32 {
@@ -20,18 +30,41 @@ fn double_float64(p: f32) -> f64 {
     (p * 2.0) as f64
 }
 
-fn int_plus_float_to_float(p1: i32, p2: f32) -> f64 {
-    ((p1 as f32) + p2) as f64
+fn int_plus_float_to_float(p1: i32, p2: f32) -> (f64, bool) {
+    let (a, b) = p1.overflowing_add(p2 as i32);
+    if b {
+        (0f64, true)
+    } else {
+        (a as f64 , false)
+    }
 }
 
-fn int_plus_float_to_int(p1: i32, p2: f32) -> i64 {
-    ((p2 as i32) + p1) as i64
+fn int_plus_float_to_int(p1: i32, p2: f32) -> (i64, bool) {
+    let (a, b) = p1.overflowing_add(p2 as i32);
+    if b {
+        (0i64, true)
+    } else {
+        (a as i64 , false)
+    }
 }
 
-fn tuple_sum(p: (i32, i32)) -> i32 {
-    p.0 + p.1
+fn tuple_sum(p: (i32, i32)) -> (i32, bool) {
+    let (a, b) = p.0.overflowing_add(p.1);
+    if b {
+        (0i32, true)
+    } else {
+        (a , false)
+    }
 }
 
-fn array_sum(p: &[i32; 3]) -> i32 {
-    p.iter().sum()
+fn array_sum(p: &[i32; 3]) -> (i32, bool) {
+    let  mut summ :i32 = 0;
+    let  mut b:bool;
+    for item in p.iter() {
+        (summ, b) = summ.overflowing_add(*item);
+        if b {
+            return  (0i32, b);
+        }
+    }
+    (summ, false)
 }
--- lesson-3/src/tests.rs
+++ lesson-3/src/tests.rs
@@ -3,12 +3,22 @@ mod tests {
 
     #[test]
     fn double_int32_test() {
-        assert_eq!(double_int32(2i32), 4i32, "ok")
+        assert_eq!(double_int32(2i32), (4i32, false), "ok")
+    }
+
+    #[test]
+    fn double_int32_overflowing_test() {
+        assert_eq!(double_int32(i32::MAX), (0i32, true), "ok")
     }
 
     #[test]
     fn double_int64_test() {
-        assert_eq!(double_int64(2i32), 4i64, "ok")
+        assert_eq!(double_int64(2i32), (4i64, false), "ok")
+    }
+
+    #[test]
+    fn double_int64_overflowing_test() {
+        assert_eq!(double_int64(i32::MAX), (0i64, true), "ok")
     }
 
     #[test]
@@ -18,26 +28,49 @@ mod tests {
 
     #[test]
     fn double_float64_test() {
-        assert_eq!(double_float64(2f32), 4f64, "ok")
+        assert_eq!(double_float64(2f32), 4f64, "ok");
     }
 
     #[test]
     fn int_plus_float_to_float_test() {
-        assert_eq!(int_plus_float_to_float(2i32, 2f32), 4f64, "ok")
+        assert_eq!(int_plus_float_to_float(2i32, 2f32), (4f64, false), "ok")
     }
 
     #[test]
+    fn int_plus_float_to_float_overflowing_test() {
+        assert_eq!(int_plus_float_to_float(i32::MAX, 2f32), (0f64, true), "ok");
+        assert_eq!(int_plus_float_to_float(2i32, f32::MAX), (0f64, true), "ok");
+    }
+
+
+    #[test]
     fn int_plus_float_to_int_test() {
-        assert_eq!(int_plus_float_to_int(2i32, 2f32), 4i64, "ok")
+        assert_eq!(int_plus_float_to_int(2i32, 2f32), (4i64, false), "ok")
+    }
+
+    #[test]
+    fn int_plus_float_to_int_overflowing_test() {
+        assert_eq!(int_plus_float_to_int(2i32, f32::MAX), (0, true), "ok");
+        assert_eq!(int_plus_float_to_int(i32::MAX, 2f32), (0, true), "ok");
     }
 
     #[test]
     fn tuple_sum_test() {
-        assert_eq!(tuple_sum((2i32, 2i32)), 4i32, "ok")
+        assert_eq!(tuple_sum((2i32, 2i32)), (4i32, false), "ok")
+    }
+
+    #[test]
+    fn tuple_sum_overflowing_test() {
+        assert_eq!(tuple_sum((2i32, i32::MAX)), (0, true), "ok")
     }
 
     #[test]
     fn array_sum_test() {
-        assert_eq!(array_sum(&[2i32, 2i32, 3i32]), 7i32, "ok")
+        assert_eq!(array_sum(&[2i32, 2i32, 7i32]), (11i32, false), "ok")
     }
+    #[test]
+    fn array_sum_overflowing_test() {
+        assert_eq!(array_sum(&[2i32, 2i32, i32::MAX]), (0, true), "ok")
+    }
+
 }
